12fd3119d8b41f1ad0fa4716f8ba9b552342336c
Piyush Yadav <piyush.y276@gmail.com>
2026-01-27 12:25:30 +0530

    Add new compound Modal for flexibilty

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,18 +4,31 @@ import Modal from "../ui/Modal";
 import { useState } from "react";
 
 export function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onCloseModal={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form"></Modal.Window>
+
+      {/* <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table"></Modal.Window> */}
+    </Modal>
   );
+
+  // const [isOpenModal, setIsOpenModal] = useState(false);
+  // return (
+  //   <div>
+  //     <Button onClick={() => setIsOpenModal((show) => !show)}>
+  //       Add new cabin
+  //     </Button>
+  //     {isOpenModal && (
+  //       <Modal onCloseModal={() => setIsOpenModal(false)}>
+  //         <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+  //       </Modal>
+  //     )}
+  //   </div>
+  // );
 }
