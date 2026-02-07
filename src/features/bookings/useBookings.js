f32147f87796f128608e741703c44bc44bb11c9e
Piyush Yadav <piyush.y276@gmail.com>
2026-02-07 20:23:58 +0530

    Add filter and sort to queryKey to cache them

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue == "all"
+      ? null
+      : { field: "status", value: filterValue, method: "eq" };
+
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const sort = { field, direction };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sort],
+    queryFn: () => getBookings({ filter, sort }),
   });
 
   return { isPending, bookings, error };
