3c3b79c4ad2cd41b70d795f410036b4a7675f93f
Piyush Yadav <piyush.y276@gmail.com>
2026-02-08 23:04:57 +0530

    Add Pagination component and fetch count from useBookings

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -7,7 +7,7 @@ import Spinner from "../../../../../../../../../Downloads/ultimate-react-course-
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isPending } = useBookings();
+  const { bookings, isPending, count } = useBookings();
 
   if (isPending) return <Spinner />;
 
@@ -34,7 +34,7 @@ function BookingTable() {
         />
       </Table>
       <Table.Footer>
-        <Pagination count={15} />
+        <Pagination count={count} />
       </Table.Footer>
     </Menus>
   );
