0457573b2c19584903140e15f36f7610c6ac67e4
Piyush Yadav <piyush.y276@gmail.com>
2026-02-26 23:20:19 +0530

    create updateCurrentUser to handle updation of userData and password in db

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,12 +1,12 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
-export async function signup({ email, password }) {
+export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
     options: {
       data: {
-        fullName: "",
+        fullName,
         avatar: "",
       },
     },
@@ -50,4 +50,44 @@ export async function logout() {
   if (error) {
     throw new Error(error.message);
   }
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update password OR fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (!avatar) return data;
+
+  // 2. Upload the avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) {
+    console.error(storageError);
+    throw new Error("Avatar could not be uploaded");
+  }
+
+  // 3. Update avatar in the user
+  const { data: updatedUser, error: dbError } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (dbError) {
+    console.error(dbError);
+    throw new Error("Avatar could not be updated in the database");
+  }
+
+  return updatedUser;
 }
