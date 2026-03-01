6f155d511b747a524c1e4fc5c70c02e7028c0e31
Piyush Yadav <piyush.y276@gmail.com>
2026-03-01 17:50:39 +0530

    create DurationChart to display a pie chart of stays per numDays

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "../../ui/Heading";
+import { Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { useDarkMode } from "../../contexts/DarkModeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -22,42 +25,42 @@ const startDataLight = [
   {
     duration: "1 night",
     value: 0,
-    color: "#ef4444",
+    fill: "#ef4444",
   },
   {
     duration: "2 nights",
     value: 0,
-    color: "#f97316",
+    fill: "#f97316",
   },
   {
     duration: "3 nights",
     value: 0,
-    color: "#eab308",
+    fill: "#eab308",
   },
   {
     duration: "4-5 nights",
     value: 0,
-    color: "#84cc16",
+    fill: "#84cc16",
   },
   {
     duration: "6-7 nights",
     value: 0,
-    color: "#22c55e",
+    fill: "#22c55e",
   },
   {
     duration: "8-14 nights",
     value: 0,
-    color: "#14b8a6",
+    fill: "#14b8a6",
   },
   {
     duration: "15-21 nights",
     value: 0,
-    color: "#3b82f6",
+    fill: "#3b82f6",
   },
   {
     duration: "21+ nights",
     value: 0,
-    color: "#a855f7",
+    fill: "#a855f7",
   },
 ];
 
@@ -65,42 +68,42 @@ const startDataDark = [
   {
     duration: "1 night",
     value: 0,
-    color: "#b91c1c",
+    fill: "#b91c1c",
   },
   {
     duration: "2 nights",
     value: 0,
-    color: "#c2410c",
+    fill: "#c2410c",
   },
   {
     duration: "3 nights",
     value: 0,
-    color: "#a16207",
+    fill: "#a16207",
   },
   {
     duration: "4-5 nights",
     value: 0,
-    color: "#4d7c0f",
+    fill: "#4d7c0f",
   },
   {
     duration: "6-7 nights",
     value: 0,
-    color: "#15803d",
+    fill: "#15803d",
   },
   {
     duration: "8-14 nights",
     value: 0,
-    color: "#0f766e",
+    fill: "#0f766e",
   },
   {
     duration: "15-21 nights",
     value: 0,
-    color: "#1d4ed8",
+    fill: "#1d4ed8",
   },
   {
     duration: "21+ nights",
     value: 0,
-    color: "#7e22ce",
+    fill: "#7e22ce",
   },
 ];
 
@@ -109,7 +112,7 @@ function prepareData(startData, stays) {
 
   function incArrayValue(arr, field) {
     return arr.map((obj) =>
-      obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
+      obj.duration === field ? { ...obj, value: obj.value + 1 } : obj,
     );
   }
 
@@ -122,7 +125,7 @@ function prepareData(startData, stays) {
       if ([4, 5].includes(num)) return incArrayValue(arr, "4-5 nights");
       if ([6, 7].includes(num)) return incArrayValue(arr, "6-7 nights");
       if (num >= 8 && num <= 14) return incArrayValue(arr, "8-14 nights");
-      if (num >= 15 && num <= 21) return incArrayValue(arr, "15-21 nights");
+      if (num >= 15 && num <= 20) return incArrayValue(arr, "15-21 nights");
       if (num >= 21) return incArrayValue(arr, "21+ nights");
       return arr;
     }, startData)
@@ -130,3 +133,40 @@ function prepareData(startData, stays) {
 
   return data;
 }
+
+function DurationChart({ confirmedStays }) {
+  const { isDarkMode } = useDarkMode();
+  const startData = isDarkMode ? startDataDark : startDataLight;
+  const pieData = prepareData(startData, confirmedStays);
+
+  return (
+    <ChartBox>
+      <Heading as="h2">Stay duration summary</Heading>
+      <ResponsiveContainer width="100%" height={240}>
+        <PieChart>
+          <Pie
+            data={pieData}
+            nameKey="duration"
+            dataKey="value"
+            innerRadius={85}
+            outerRadius={110}
+            cx="40%"
+            cy="50%"
+            paddingAngle={3}
+          />
+          <Tooltip />
+          <Legend
+            verticalAlign="middle"
+            align="right"
+            width="30%"
+            layout="vertical"
+            iconType="circle"
+            iconSize={15}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+  );
+}
+
+export default DurationChart;
